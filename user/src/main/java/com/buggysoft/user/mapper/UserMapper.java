d0ae5e34b659e9a98a1a877553d0b32d7e87038d
AveryYay <mf3332@columbia.edu>
2024-09-26 22:38:13 -0400

    Added UserMapper

--- user/src/main/java/com/buggysoft/user/mapper/UserMapper.java
+++ user/src/main/java/com/buggysoft/user/mapper/UserMapper.java
@@ -1,4 +1,7 @@
 package com.buggysoft.user.mapper;
 
-public interface UserMapper{
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.buggysoft.user.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
 }
